f28771ebbf048bbf3fda41c19741f29bc3666701
Dotos <yy.liu@foxmail.com>
2017-08-08 19:51:27 +0800

    update at function

--- src/Valine.js
+++ src/Valine.js
@@ -2,6 +2,7 @@ require('./Valine.scss');
 import snarkdown from 'snarkdown';
 
 const defaultComment = {
+    at: '',
     comment: '',
     rid: '',
     nick: 'unknow',
@@ -59,11 +60,11 @@ class Valine {
         }
         _root.element.classList.add('valine');
         let placeholder = option.placeholder || 'ヾﾉ≧∀≦)o来啊，快活啊!';
-        let eleHTML = `<div class="vwrap"><div class="vedit"><textarea class="veditor vinput" placeholder="${placeholder}"></textarea></div><div class="vcontrol"><div class='vident'><input placeholder="称呼" class="vnick vinput" type="text"><input placeholder="网址" class="vlink vinput" type="text"><input placeholder="邮箱" class="vmail vinput" type="text"></div><div class="vright"><button type="button" class="vsubmit vbtn">回复</button></div></div></div><div class="pd5 txt-right power">Powered By <a href="https://github.com/xCss/Valine" target="_blank">Valine</a></div><ul class="vlist"><li class="vloading"></li><li class="vempty"></li></ul>`;
+        let eleHTML = `<div class="vwrap"><div class="vedit"><textarea class="veditor vinput" placeholder="${placeholder}"></textarea></div><div class="vcontrol"><div class='vident'><input placeholder="称呼" class="vnick vinput" type="text"><input placeholder="网址(http://)" class="vlink vinput" type="text"><input placeholder="邮箱" class="vmail vinput" type="text"></div><div class="vright"><button type="button" class="vsubmit vbtn">回复</button></div></div><div style="display:none;" class="vmark"><div class="valert txt-center"><div class="vtext"></div><div><button class="vcancel vbtn">我再看看</button><button class="vsure vbtn">继续提交</button></div></div></div></div><div class="pd5 txt-right power">Powered By <a href="https://github.com/xCss/Valine" target="_blank">Valine</a></div><ul class="vlist"><li class="vloading"></li><li class="vempty"></li></ul>`;
         _root.element.innerHTML = eleHTML;
 
         // loading
-        let _spinner = `<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>`;
+        let _spinner = `<div class="spinner"><div class="r1"></div><div class="r2"></div><div class="r3"></div><div class="r4"></div><div class="r5"></div></div>`;
         let vloading = _root.element.querySelector('.vloading');
         vloading.innerHTML = _spinner;
         // loading control
@@ -104,7 +105,7 @@ class Valine {
                     let _vcard = document.createElement('li');
                     _vcard.setAttribute('class', 'vcard');
                     _vcard.setAttribute('data-id', item.id);
-                    _vcard.innerHTML = `<div class="vhead"><a href="${convertUrl(item.get('link'))}" target="_blank" data-id="${item.id}">${item.get("nick")}</a><span class="vtime">${dateFormat(item.get("updatedAt"))}</span><span rid='${item.id}' at='${item.get('nick')}' class="vat">回复</span></div><div class="vcomment">${item.get("comment")}</div>`;
+                    _vcard.innerHTML = `<div class="vhead" id='${item.id}'><a href="${item.get('link') || item.get('mail')}" target="_blank" >${item.get("nick")}</a><span class="vtime">${dateFormat(item.get("updatedAt"))}</span><span rid='${item.id}' at='${item.get('nick')}' class="vat">回复</span></div><div class="vcomment">${item.get("comment")}</div>`;
                     let _vlist = _root.element.querySelector('.vlist');
                     let _vlis = _vlist.querySelectorAll('li');
                     let _vat = _vcard.querySelector('.vat');
@@ -152,16 +153,40 @@ class Valine {
             }
             defaultComment['nick'] = 'unknow';
             defaultComment['rid'] = '';
+            defaultComment['at'] = '';
         }
 
         // at event
         _root.bindAt = (el) => {
             el.addEventListener('click', (e) => {
                 let at = el.getAttribute('at');
-                // let rid = el.getAttribute('rid');
+                let rid = el.getAttribute('rid');
+                defaultComment['at'] = at;
+                defaultComment['rid'] = rid;
                 inputs['comment'].value = `@${at} ，`;
                 inputs['comment'].focus();
             })
+        }
+
+        // alert
+        let _mark = _root.element.querySelector('.vmark');
+        let _atx = _mark.querySelector('.vtext');
+        let _vok = _mark.querySelector('.vsure');
+        _mark.querySelector('.vcancel').addEventListener('click', function(e) {
+            _root.alert.hide();
+        });
+        _root.alert = {
+            show(txt, cb) {
+                _atx.innerHTML = txt;
+                _mark.setAttribute('style', 'display:block;');
+                _vok.addEventListener('click', function(e) {
+                    cb && cb()
+                    _root.alert.hide();
+                })
+            },
+            hide() {
+                _mark.setAttribute('style', 'display:none;');
+            }
         }
 
         // submit
@@ -171,13 +196,28 @@ class Valine {
                 inputs['comment'].focus();
                 return;
             }
+            let idx = defaultComment.comment.indexOf(defaultComment.at);
+            if (idx > -1) {
+                let at = `<a href='#${defaultComment.rid}'>${defaultComment.at}</a>`;
+                defaultComment.comment.replace(defaultComment.at, at);
+            }
             if (defaultComment.nick == '') {
                 defaultComment['nick'] = '小调皮';
             }
             defaultComment.comment = snarkdown(defaultComment.comment);
-            defaultComment.link = convertUrl(defaultComment.link);
-            _root.loading.show();
+            if (!verify.mail(defaultComment.mail) && !verify.link(defaultComment.link)) {
+                _root.alert.show('您的网址和邮箱格式不正确, 是否继续提交?', commit)
+            } else if (!verify.mail(defaultComment.mail)) {
+                _root.alert.show('您的邮箱格式不正确, 是否继续提交?', commit)
+            } else if (!verify.link(defaultComment.link)) {
+                _root.alert.show('您的网址格式不正确, 是否继续提交?', commit)
+            } else {
+                commit()
+            }
+        })
 
+        let commit = () => {
+            _root.loading.show();
             // 声明类型
             let Ct = _root.v.Object.extend('Comment');
             // 新建对象
@@ -190,7 +230,7 @@ class Valine {
                 let _vcard = document.createElement('li');
                 _vcard.setAttribute('class', 'vcard');
                 _vcard.setAttribute('data-id', ret.id);
-                _vcard.innerHTML = `<div class="vhead"><a href="${convertUrl(ret.get('link'))}" target="_blank" data-id="${ret.id}">${ret.get('nick')}</a><span class="vtime">${dateFormat(ret.get("updatedAt"))}</span><span rid='${ret.id}' at='${ret.get('nick')}' class="vat">回复</span></div><div class="vcomment">${ret.get('comment')}</div>`;
+                _vcard.innerHTML = `<div class="vhead" id='${ret.id}'><a href="${ret.get('link') || ret.get('mail')}" target="_blank" >${ret.get('nick')}</a><span class="vtime">${dateFormat(ret.get("updatedAt"))}</span><span rid='${ret.id}' at='${ret.get('nick')}' class="vat">回复</span></div><div class="vcomment">${ret.get('comment')}</div>`;
                 let _vlist = _root.element.querySelector('.vlist');
                 let _vlis = _vlist.querySelectorAll('li');
                 let _vat = _vcard.querySelector('.vat');
@@ -203,10 +243,21 @@ class Valine {
             }).catch(ex => {
                 _root.loading.hide();
             })
-        })
+        }
     }
 
 }
+
+const verify = {
+    mail(m) {
+        return /^[a-z]([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}([\.][a-z]{2})?$/i.test(m);
+    },
+    link(l) {
+        l = /^(http|https)/.test(l) ? l : `http://${l}`;
+        return /(((^https?:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)$/g.test(l);
+    }
+}
+
 const HtmlUtil = {
     /**
      * HTML转码
@@ -214,8 +265,7 @@ const HtmlUtil = {
      * @return {String} result
      */
     encode(str) {
-        if (str.length == 0) return "";
-        return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/ /g, "&nbsp;").replace(/\'/g, "&#39;").replace(/\"/g, "&quot;");
+        return !!str ? str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/ /g, "&nbsp;").replace(/\'/g, "&#39;").replace(/\"/g, "&quot;") : '';
     },
     /**
      * HTML解码
@@ -223,8 +273,7 @@ const HtmlUtil = {
      * @return {String} result
      */
     decode(str) {
-        if (str.length == 0) return "";
-        return str.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&nbsp;/g, " ").replace(/&#39;/g, "\'").replace(/&quot;/g, "\"");
+        return !!str ? str.replace(/&amp;/g, "&").replace(/&lt;/g, "<").replace(/&gt;/g, ">").replace(/&nbsp;/g, " ").replace(/&#39;/g, "\'").replace(/&quot;/g, "\"") : '';
     }
 };
 
