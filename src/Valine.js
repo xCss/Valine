a46c5fd562b337e2af37dd80e282636d1301a145
Dotos <yy.liu@foxmail.com>
2017-08-09 19:10:58 +0800

    Security issues for repairing permissions

--- src/Valine.js
+++ src/Valine.js
@@ -230,7 +230,7 @@ class Valine {
             }
             let acl = new _root.v.ACL();
             acl.setPublicReadAccess(true);
-            acl.setPublicWriteAccess(true);
+            acl.setPublicWriteAccess(false);
             comment.setACL(acl);
             comment.save().then((ret) => {
                 let _vcard = document.createElement('li');
