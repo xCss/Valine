7d0746c6676efdb97fa3a8fefd1753033ce55425
dotos <yy.liu@foxmail.com>
2017-08-18 07:26:57 +0800

    fix style

--- src/Valine.js
+++ src/Valine.js
@@ -31,7 +31,7 @@ class Valine {
     constructor(option) {
         let _root = this;
         // version
-        _root.version = '1.1.5';
+        _root.version = '1.1.6';
 
         _root.md5 = md5;
         // Valine init
