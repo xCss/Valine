a4bc0b1e796866a7576303c4cc1882a1ba83d8ad
Dotos <yy.liu@foxmail.com>
2017-08-22 17:40:25 +0800

    new version

--- src/Valine.js
+++ src/Valine.js
@@ -31,7 +31,7 @@ class Valine {
     constructor(option) {
         let _root = this;
         // version
-        _root.version = '1.1.6';
+        _root.version = '1.1.7-beta';
 
         _root.md5 = md5;
         // Valine init
