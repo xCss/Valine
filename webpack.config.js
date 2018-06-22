b8f62a1e4417a57f7968d2faa65412b4ca8d6166
xCss <yy.liu@foxmail.com>
2018-06-23 00:19:27 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,82 @@
-var webpack = require('webpack');
-var path = require('path');
-var autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const path = require('path');
+const autoprefixer = require('autoprefixer');
+const libraryName = 'Valine';
+const version = require('./package.json').version
+const ROOT_PATH = path.resolve(__dirname);
+const APP_PATH = path.resolve(ROOT_PATH, 'src');
+let BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
 
-var libraryName = 'Valine';
-var ROOT_PATH = path.resolve(__dirname);
-var APP_PATH = path.resolve(ROOT_PATH, 'src');
-var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
+const plugins = [];
+const banner =
+  'Valine v' + version + '\n' +
+  '(c) 2017-' + new Date().getFullYear() + ' xCss\n' +
+  'Released under the GPL-2.0 License.' ;
 
-var plugins = [];
+const TARGET = process.env
+if(TARGET.NODE_ENV == 'production'){
+    BUILD_PATH = path.resolve(ROOT_PATH, 'dist/'+version);
+}
 
 module.exports = env => {
     var dev = env && env.dev || false
     if (!dev) {
+        plugins.push(new webpack.BannerPlugin(banner))
+        plugins.push(new webpack.LoaderOptionsPlugin({
+            minimize: true
+        }));
         plugins.push(
             new webpack.optimize.UglifyJsPlugin({
-                // 最紧凑的输出
-                beautify: false,
-                // 删除所有的注释
-                comments: false,
-                sourceMap: false,
                 compress: {
-                    // 在UglifyJs删除没有用到的代码时不输出警告  
                     warnings: false,
-                    // 删除所有的 `console` 语句
-                    drop_console: true,
+                    // 内嵌定义了但是只用到一次的变量
+                    collapse_vars: true,
+                    // 提取出出现多次但是没有定义成变量去引用的静态值
+                    reduce_vars: true,
+                    drop_console: true
                 },
-                mangle:{
+                sourceMap:false,
+                uglifyOptions:{
+                    output:{
+                        // 删除所有的注释
+                        comments: false,
+                        beautify:false
+                    },
                     safari10:true
                 }
             })
         );
-        plugins.push(new webpack.LoaderOptionsPlugin({
-            minimize: true
-        }));
     } else {
+        plugins.push(new webpack.LoaderOptionsPlugin())
         plugins.push(new webpack.NamedModulesPlugin())
         plugins.push(new webpack.HotModuleReplacementPlugin())
     }
     return {
         entry: {
-            Valine: ['./src/Valine.scss', './src/Valine.js'],
-            'ValinePure': './src/Valine.js',
+            Valine: ['./src/index.scss', './src/index.js'],
+            'Valine.Pure': './src/index.js'
             //'Valine.locales': './src/Valine.locales.js',
-            detect: './src/detect.js',
-            escape: './src/escape.js'
+            // detect: './src/utils/detect.js',
+            // escape: './src/utils/escape.js'
         },
         output: {
             path: BUILD_PATH,
             filename: '[name].min.js',
-            library: '[name]',
-            libraryTarget: 'umd',
-            umdNamedDefine: true
+            library: libraryName,
+            libraryTarget: 'umd'
         },
-
+        resolve: {
+            extensions: ['.js','.json', '.jsx','.css','.scss']
+        },
         devtool: 'cheap-module-source-map',
 
         devServer: {
             hot: true,
             port: 8088,
             inline: true,
-            progress: true,
-            host: '0.0.0.0',
-            publicPath: "/dist/",
-            historyApiFallback: true,
+            host: '127.0.0.1',
+            publicPath:'/dist/',
+            compress: true,
             stats: 'errors-only', //只在发生错误时输出
             overlay: { //当有编译错误或者警告的时候显示一个全屏overlay
                 errors: true,
@@ -73,8 +87,13 @@ module.exports = env => {
         module: {
             rules: [{
                 test: /\.js$/,
-                loader: 'babel-loader',
-                include: [APP_PATH],
+                use: {
+                    loader:'babel-loader',
+                    options:{
+                        cacheDirectory:true,
+                        compact:true
+                    }
+                },
                 exclude: /node_modules/
             }, {
                 test: /\.scss$/,
@@ -93,8 +112,8 @@ module.exports = env => {
                     'postcss-loader'
                 ]
             }, {
-                test: /\.(png|jpg|gif)$/,
-                use: ['url-loader?limit=8192']
+                test: /\.(png|jpg|gif|svg)$/,
+                use: ['url-loader?limit=1024*10']
                 //loader: 'url-loader?limit=40000'
             }]
         },
